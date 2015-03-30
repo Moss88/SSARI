20d7cf46f7a4afd8257a8a339e34e5f639737cdd
lrpierce <lukepierc@gmail.com>
2015-03-30 17:09:44 -0500

    Added consolidated header file Closed #6

---
+++ include/ssari.h
@@ -0,0 +1,15 @@
+#ifndef SSARI
+#define SSARI
+
+#include "CConstant.h"
+#include "Constraint.h"
+#include "COperand.h"
+#include "COperator.h"
+#include "CValue.h"
+#include "CVar.h"
+#include "RegisterFile.h"
+#include "RegisterFileReader.h"
+#include "RegisterFileWriter.h"
+
+#endif // SSARI
+
